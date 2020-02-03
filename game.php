5c604037ba5eab871cc39759b7984b64ecabbc7d
tine <tinesaenen@hotmail.be>
2020-02-03 16:42:29 +0100

    game.php eerste commit

---
+++ game.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hangman start</title>
+</head>
+<body>
+
+<div class="hangman_images">
+  <img src="https://source.unsplash.com/300x300/?cat" alt="">
+</div>
+  
+</body>
+</html>
