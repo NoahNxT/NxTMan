7788c47722fccd79b76f2073383c33dd03fbf1fa
tine <tinesaenen@hotmail.be>
2020-02-06 18:52:18 +0100

    hangman test images toegevoegd, id's gegeven aan images en key's

--- game.php
+++ game.php
@@ -11,36 +11,49 @@
 
 <div class="gamePageContainer">
   <div class="hangmanImages">
-    <img src="https://source.unsplash.com/100x100/?cat" alt="">
+    <img id="1" class="hangmanImages--img1" src="img/hangmanImg/1.jpg" alt="" width=200 height=200>
+    <img id="2" class="hangmanImages--img2" src="img/hangmanImg/2.jpg" alt="" width=200 height=200>
+    <img id="3" class="hangmanImages--img3" src="img/hangmanImg/3.jpg" alt="" width=200 height=200>
+    <img id="4" class="hangmanImages--img4" src="img/hangmanImg/4.jpg" alt="" width=200 height=200>
+    <img id="5" class="hangmanImages--img5" src="img/hangmanImg/5.jpg" alt="" width=200 height=200>
+    <img id="6" class="hangmanImages--img6" src="img/hangmanImg/6.jpg" alt="" width=200 height=200>
+    <img id="7" class="hangmanImages--img7" src="img/hangmanImg/7.jpg" alt="" width=200 height=200>
+    <img id="8" class="hangmanImages--img8" src="img/hangmanImg/8.jpg" alt="" width=200 height=200>
+    <img id="9" class="hangmanImages--img9" src="img/hangmanImg/9.jpg" alt="" width=200 height=200>
+    <img id="10" class="hangmanImages--img10" src="img/hangmanImg/10.jpg" alt="" width=200 height=200>
+  </div>
+
+  <div class="gridOutput">
+
   </div>
   
-  <div class="keyContainer">
-    <button value="a" class="key">A</button>
-    <button value="b" class="key">B</button>
-    <button value="c" class="key">C</button>
-    <button value="d" class="key">D</button>
-    <button value="e" class="key">E</button>
-    <button value="f" class="key">F</button>
-    <button value="g" class="key">G</button>
-    <button value="h" class="key">H</button>
-    <button value="i" class="key">I</button>
-    <button value="j" class="key">J</button>
-    <button value="k" class="key">K</button>
-    <button value="l" class="key">L</button>
-    <button value="m" class="key">M</button>
-    <button value="n" class="key">N</button>
-    <button value="o" class="key">O</button>
-    <button value="p" class="key">P</button>
-    <button value="q" class="key">Q</button>
-    <button value="r" class="key">R</button>
-    <button value="s" class="key">S</button>
-    <button value="t" class="key">T</button>
-    <button value="u" class="key">U</button>
-    <button value="v" class="key">V</button>
-    <button value="w" class="key">W</button>
-    <button value="x" class="key">X</button>
-    <button value="y" class="key">Y</button>    
-    <button value="z" class="key">Z</button>
+  <div class="gridInput">
+    <button id="a" class="key">A</button>
+    <button id="b" class="key">B</button>
+    <button id="c" class="key">C</button>
+    <button id="d" class="key">D</button>
+    <button id="e" class="key">E</button>
+    <button id="f" class="key">F</button>
+    <button id="g" class="key">G</button>
+    <button id="h" class="key">H</button>
+    <button id="i" class="key">I</button>
+    <button id="j" class="key">J</button>
+    <button id="k" class="key">K</button>
+    <button id="l" class="key">L</button>
+    <button id="m" class="key">M</button>
+    <button id="n" class="key">N</button>
+    <button id="o" class="key">O</button>
+    <button id="p" class="key">P</button>
+    <button id="q" class="key">Q</button>
+    <button id="r" class="key">R</button>
+    <button id="s" class="key">S</button>
+    <button id="t" class="key">T</button>
+    <button id="u" class="key">U</button>
+    <button id="v" class="key">V</button>
+    <button id="w" class="key">W</button>
+    <button id="x" class="key">X</button>
+    <button id="y" class="key">Y</button>    
+    <button id="z" class="key">Z</button>
   </div> 
 
 </div>
