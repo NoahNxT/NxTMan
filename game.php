fd577b4ae70924a6f1409489ca4196a4bc853ff5
tine <tinesaenen@hotmail.be>
2020-02-06 18:03:49 +0100

    keyboard aangemaakt, alle letters values gegeven game.php

--- game.php
+++ game.php
@@ -4,13 +4,46 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css" />
   <title>Hangman start</title>
 </head>
 <body>
 
-<div class="hangman_images">
-  <img src="https://source.unsplash.com/300x300/?cat" alt="">
-</div>
+<div class="gamePageContainer">
+  <div class="hangmanImages">
+    <img src="https://source.unsplash.com/100x100/?cat" alt="">
+  </div>
   
+  <div class="keyContainer">
+    <button value="a" class="key">A</button>
+    <button value="b" class="key">B</button>
+    <button value="c" class="key">C</button>
+    <button value="d" class="key">D</button>
+    <button value="e" class="key">E</button>
+    <button value="f" class="key">F</button>
+    <button value="g" class="key">G</button>
+    <button value="h" class="key">H</button>
+    <button value="i" class="key">I</button>
+    <button value="j" class="key">J</button>
+    <button value="k" class="key">K</button>
+    <button value="l" class="key">L</button>
+    <button value="m" class="key">M</button>
+    <button value="n" class="key">N</button>
+    <button value="o" class="key">O</button>
+    <button value="p" class="key">P</button>
+    <button value="q" class="key">Q</button>
+    <button value="r" class="key">R</button>
+    <button value="s" class="key">S</button>
+    <button value="t" class="key">T</button>
+    <button value="u" class="key">U</button>
+    <button value="v" class="key">V</button>
+    <button value="w" class="key">W</button>
+    <button value="x" class="key">X</button>
+    <button value="y" class="key">Y</button>    
+    <button value="z" class="key">Z</button>
+  </div> 
+
+</div>
+
 </body>
 </html>
