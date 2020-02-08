8894837d57b2344936392bb6571060a324364e6e
Noah Gillard <noah.gillard@student.kdg.be>
2020-02-08 18:57:15 +0100

    onclick event letter buttons

--- game.php
+++ game.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="style.css" />
   <title>Hangman</title>
+  
 </head>
 <body>
 
@@ -14,16 +15,16 @@
   if (isset($_POST["chosen_word"]))
   {
     $chosen_word = $_POST["chosen_word"];
-    // echo $chosen_word;
+     echo $chosen_word;
   }
 
   //Count amount of characters in chosen word
   $amount_Letters = strlen($chosen_word);
-  //echo $amount_Letters;
+  echo $amount_Letters;
 
   //split every letter individual in array
   $split_Word_Arr = str_split($chosen_word);
-  //print_r($split_Word_Arr);
+  print_r($split_Word_Arr);
 ?>
 
 <div class="gamePageContainer">
@@ -49,39 +50,35 @@
     ?>    
   </div>
   
-  <form method="post">
-    <div class="gridInput">
-      <button id="a" class="key">A</button>
-      <button id="b" class="key">B</button>
-      <button id="c" class="key">C</button>
-      <button id="d" class="key">D</button>
-      <button id="e" class="key">E</button>
-      <button id="f" class="key">F</button>
-      <button id="g" class="key">G</button>
-      <button id="h" class="key">H</button>
-      <button id="i" class="key">I</button>
-      <button id="j" class="key">J</button>
-      <button id="k" class="key">K</button>
-      <button id="l" class="key">L</button>
-      <button id="m" class="key">M</button>
-      <button id="n" class="key">N</button>
-      <button id="o" class="key">O</button>
-      <button id="p" class="key">P</button>
-      <button id="q" class="key">Q</button>
-      <button id="r" class="key">R</button>
-      <button id="s" class="key">S</button>
-      <button id="t" class="key">T</button>
-      <button id="u" class="key">U</button>
-      <button id="v" class="key">V</button>
-      <button id="w" class="key">W</button>
-      <button id="x" class="key">X</button>
-      <button id="y" class="key">Y</button>    
-      <button id="z" class="key">Z</button>
-    </div> 
-  </form>
-
-
-
+  
+  <div class="gridInput">
+    <button onclick="window.location.href = '?letter=a';" id="a" class="key">A</button>
+    <button onclick="window.location.href = '?letter=b';" id="b" class="key">B</button>
+    <button onclick="window.location.href = '?letter=c';" id="c" class="key">C</button>
+    <button onclick="window.location.href = '?letter=d';" id="d" class="key">D</button>
+    <button onclick="window.location.href = '?letter=e';" id="e" class="key">E</button>
+    <button onclick="window.location.href = '?letter=f';" id="f" class="key">F</button>
+    <button onclick="window.location.href = '?letter=g';" id="g" class="key">G</button>
+    <button onclick="window.location.href = '?letter=h';" id="h" class="key">H</button>
+    <button onclick="window.location.href = '?letter=i';" id="i" class="key">I</button>
+    <button onclick="window.location.href = '?letter=j';" id="j" class="key">J</button>
+    <button onclick="window.location.href = '?letter=k';" id="k" class="key">K</button>
+    <button onclick="window.location.href = '?letter=l';" id="l" class="key">L</button>
+    <button onclick="window.location.href = '?letter=m';" id="m" class="key">M</button>
+    <button onclick="window.location.href = '?letter=n';" id="n" class="key">N</button>
+    <button onclick="window.location.href = '?letter=o';" id="o" class="key">O</button>
+    <button onclick="window.location.href = '?letter=p';" id="p" class="key">P</button>
+    <button onclick="window.location.href = '?letter=q';" id="q" class="key">Q</button>
+    <button onclick="window.location.href = '?letter=r';" id="r" class="key">R</button>
+    <button onclick="window.location.href = '?letter=s';" id="s" class="key">S</button>
+    <button onclick="window.location.href = '?letter=t';" id="t" class="key">T</button>
+    <button onclick="window.location.href = '?letter=u';" id="u" class="key">U</button>
+    <button onclick="window.location.href = '?letter=v';" id="v" class="key">V</button>
+    <button onclick="window.location.href = '?letter=w';" id="w" class="key">W</button>
+    <button onclick="window.location.href = '?letter=x';" id="x" class="key">X</button>
+    <button onclick="window.location.href = '?letter=y';" id="y" class="key">Y</button>    
+    <button onclick="window.location.href = '?letter=z';" id="z" class="key">Z</button>
+  </div> 
 </div>
 
 
