63fe2e252a1287472c43a3ede7f424fe2dd4508e
Noah Gillard <noah.gillard@student.kdg.be>
2020-02-13 18:48:08 +0100

    game.php loops for keys

--- game.php
+++ game.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,60 +8,48 @@
   <link rel="stylesheet" href="style.css" />
   <title>Hangman</title>
 </head>
+
+<?php
+include 'ButtonAction.php';
+include 'functionality.php';
+?>
+
 <body>
+  <div class="gamePageContainer">
+    <div class="hangmanImages">
+      <img id="1" class="hangmanImages--img1" src="img/hangmanImg/1.png" alt="" width=200 height=200>
+      <img id="2" class="hangmanImages--img2" src="img/hangmanImg/2.png" alt="" width=200 height=200>
+      <img id="3" class="hangmanImages--img3" src="img/hangmanImg/3.png" alt="" width=200 height=200>
+      <img id="4" class="hangmanImages--img4" src="img/hangmanImg/4.png" alt="" width=200 height=200>
+      <img id="5" class="hangmanImages--img5" src="img/hangmanImg/5.png" alt="" width=200 height=200>
+      <img id="6" class="hangmanImages--img6" src="img/hangmanImg/6.png" alt="" width=200 height=200>
+      <img id="7" class="hangmanImages--img7" src="img/hangmanImg/7.png" alt="" width=200 height=200>
+      <img id="8" class="hangmanImages--img8" src="img/hangmanImg/8.png" alt="" width=200 height=200>
+      <img id="9" class="hangmanImages--img9" src="img/hangmanImg/9.png" alt="" width=200 height=200>
+      <img id="10" class="hangmanImages--img10" src="img/hangmanImg/10.png" alt="" width=200 height=200>
+    </div>
 
-<div class="gamePageContainer">
-  <div class="hangmanImages">
-    <img id="1" class="hangmanImages--img1" src="img/hangmanImg/1.jpg" alt="" width=200 height=200>
-    <img id="2" class="hangmanImages--img2" src="img/hangmanImg/2.jpg" alt="" width=200 height=200>
-    <img id="3" class="hangmanImages--img3" src="img/hangmanImg/3.jpg" alt="" width=200 height=200>
-    <img id="4" class="hangmanImages--img4" src="img/hangmanImg/4.jpg" alt="" width=200 height=200>
-    <img id="5" class="hangmanImages--img5" src="img/hangmanImg/5.jpg" alt="" width=200 height=200>
-    <img id="6" class="hangmanImages--img6" src="img/hangmanImg/6.jpg" alt="" width=200 height=200>
-    <img id="7" class="hangmanImages--img7" src="img/hangmanImg/7.jpg" alt="" width=200 height=200>
-    <img id="8" class="hangmanImages--img8" src="img/hangmanImg/8.jpg" alt="" width=200 height=200>
-    <img id="9" class="hangmanImages--img9" src="img/hangmanImg/9.jpg" alt="" width=200 height=200>
-    <img id="10" class="hangmanImages--img10" src="img/hangmanImg/10.jpg" alt="" width=200 height=200>
+    <div class="gridOutput">
+      <?php
+      for ($x = 1; $x <= $amount_Letters['LENGTH']; $x++) {
+        echo '<div class="guessedLetter" id=' . $x . ' >_</div>';
+      }
+      ?>
+    </div>
+
+    <form action="" method="post">
+      <div class="gridInput">
+        <input type="submit" name="a" value="A" class="key">
+        <?php
+        $alphas = range('A', 'Z');
+        for ($x = 1; $x < count($alphas); $x++) {
+          echo '<input type="submit" name="' . $alphas[$x] . '" value="' . $alphas[$x] . '" class="key">';
+        }
+        ?>
+      </div>
+    </form>
+
   </div>
-
-  <div class="gridOutput">
-    <div class="guessedLetter">K</div>
-    <div class="guessedLetter">I</div>
-    <div class="guessedLetter">K</div>
-    <div class="guessedLetter">K</div>
-    <div class="guessedLetter">E</div>
-    <div class="guessedLetter">R</div>
-  </div>
-  
-  <div class="gridInput">
-    <button id="a" class="key">A</button>
-    <button id="b" class="key">B</button>
-    <button id="c" class="key">C</button>
-    <button id="d" class="key">D</button>
-    <button id="e" class="key">E</button>
-    <button id="f" class="key">F</button>
-    <button id="g" class="key">G</button>
-    <button id="h" class="key">H</button>
-    <button id="i" class="key">I</button>
-    <button id="j" class="key">J</button>
-    <button id="k" class="key">K</button>
-    <button id="l" class="key">L</button>
-    <button id="m" class="key">M</button>
-    <button id="n" class="key">N</button>
-    <button id="o" class="key">O</button>
-    <button id="p" class="key">P</button>
-    <button id="q" class="key">Q</button>
-    <button id="r" class="key">R</button>
-    <button id="s" class="key">S</button>
-    <button id="t" class="key">T</button>
-    <button id="u" class="key">U</button>
-    <button id="v" class="key">V</button>
-    <button id="w" class="key">W</button>
-    <button id="x" class="key">X</button>
-    <button id="y" class="key">Y</button>    
-    <button id="z" class="key">Z</button>
-  </div> 
-</div>
-
 </body>
+
 </html>
