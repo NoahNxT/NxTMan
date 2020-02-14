783eae39a76961716cbd7b4bc4b7e940a6c64272
Noah Gillard <noah.gillard@student.kdg.be>
2020-02-14 01:24:03 +0100

    Fix Push_array + check empty

--- ButtonAction.php
+++ ButtonAction.php
@@ -7,13 +7,64 @@
     //POST-request van aangeklikte knop in variable zetten
         if( isset( $_POST[$_SESSION["alphas"][$x]] ))
         {
+            
+            for ($x = 0; $x < count($_SESSION["alphas"]); $x++) 
+            {
             //POST-request van aangeklikte knop in variable zetten
+                if( isset( $_POST[$_SESSION["alphas"][$x]] ))
+                {
+                    //POST-request van aangeklikte knop in variable zetten
+                    $_SESSION["chosen_Letter"] = $_POST[$_SESSION["alphas"][$x]];
+                    
+                    if(empty($_SESSION["Letters"]))
+                    {
+                        $_SESSION["Letters"][0] = $_SESSION["chosen_Letter"];
+                    }
+
+                    if(!empty($_SESSION["Letters"]))
+                    {
+                        array_push($_SESSION["Letters"],$_SESSION["chosen_Letter"]);
+                    
+
+                        $result = array_intersect($_SESSION["splitArray_word"], $_SESSION["Letters"]);
+                        
+                        echo print_r($_SESSION["splitArray_word"]).'<br>';
+                        echo print_r($_SESSION["Letters"]).'<br>';
+                        echo print_r($result).'<br>';
+                    }
+                    
+                    /*for ($x = 0; $x <= count($result); $x++) 
+                    {
+                        if ($result[$x] == TRUE)
+                        {
+                        echo $_SESSION["splitArray_word"][$x];
+                        }else{
+                            echo '_';
+                        }
+                    }
+                    */
+
+                }
+            }
             
-            
-            $_SESSION["chosen_Letter"] = $_POST[$_SESSION["alphas"][$x]];
-            
-                array_push($_SESSION["Letters"],$_SESSION["chosen_Letter"]);
-                print_r($_SESSION["Letters"]);
+
+           
+           
+            // $Compare = array_intersect($_SESSION["splitArray_word"], $_SESSION["Letters"]);
+            //print_r($_SESSION["Letters"]);
+            //print_r($_SESSION["splitArray_word"]);
+            //print_r($Compare);
+            /*
+            for ($x = 0; $x < count($_SESSION["splitArray_word"]); $x++) 
+            {
+                if ($Compare[$x] == $_SESSION["splitArray_word"][$x])
+                {
+                echo $_SESSION["splitArray_word"][$x].'-';
+                }else{
+                echo '@';
+                }
+            }   
+               */ 
             
 
         }
@@ -23,28 +74,9 @@
     
     
 
-            /*//Parse Latest added word
-            $sql3 = "SELECT `WORD` FROM `session` ORDER BY ID DESC LIMIT 1";
-            $result3 = mysqli_query($conn, $sql3);
-            //resultaat van woord is een array
-            $word_As_Array = mysqli_fetch_assoc($result3);
-            //woord dat in array staat omgevormd naar een string
-            $word_As_String = implode("",$word_As_Array);
-            //Split string in array met elke letter apart in volgorde
-            $word_Split_Array = str_split($word_As_String);
-
-
-           /* $Compare = array_intersect($word_Split_Array, $letters_As_Array);
-            for ($x = 0; $x <= count($Compare); $x++) 
-            {
-                if ($Compare[$x] == TRUE)
-                {
-                echo $word_Split_Array[$x].'-';
-                }else{
-                echo '@';
-                }
-            }
-            */
+          
+            
+            
             
             
 
--- variables.php
+++ variables.php
@@ -4,13 +4,11 @@
 //Post voor woord in variable 
 if (isset($_POST["chosen_word"]))
 {
-    $_SESSION["chosen_word"] = $_POST["chosen_word"];
+    $_SESSION["chosen_word"] = strtoupper($_POST["chosen_word"]);
     $_SESSION["amount_Letter"] = strlen($_SESSION["chosen_word"]);
+    $_SESSION["splitArray_word"] = str_split($_SESSION["chosen_word"]);
 }
 
 $_SESSION["alphas"] = range('A', 'Z');
-
-//$_SESSION["Letters"] = array();
-  
 
 ?>
