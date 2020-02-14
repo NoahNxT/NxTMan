72390ddf2c66ee9d981b83fc840dc8424e6fe603
tine <tinesaenen@hotmail.be>
2020-02-14 17:22:33 +0100

    hangman images grootte gegeven responsive

--- game.php
+++ game.php
@@ -17,16 +17,16 @@ include 'functionality.php';
 <body>
   <div class="gamePageContainer">
     <div class="hangmanImages">
-      <img id="1" class="hangmanImages--img1" src="img/hangmanImg/1.png" alt="" width=200 height=200>
-      <img id="2" class="hangmanImages--img2" src="img/hangmanImg/2.png" alt="" width=200 height=200>
-      <img id="3" class="hangmanImages--img3" src="img/hangmanImg/3.png" alt="" width=200 height=200>
-      <img id="4" class="hangmanImages--img4" src="img/hangmanImg/4.png" alt="" width=200 height=200>
-      <img id="5" class="hangmanImages--img5" src="img/hangmanImg/5.png" alt="" width=200 height=200>
-      <img id="6" class="hangmanImages--img6" src="img/hangmanImg/6.png" alt="" width=200 height=200>
-      <img id="7" class="hangmanImages--img7" src="img/hangmanImg/7.png" alt="" width=200 height=200>
-      <img id="8" class="hangmanImages--img8" src="img/hangmanImg/8.png" alt="" width=200 height=200>
-      <img id="9" class="hangmanImages--img9" src="img/hangmanImg/9.png" alt="" width=200 height=200>
-      <img id="10" class="hangmanImages--img10" src="img/hangmanImg/10.png" alt="" width=200 height=200>
+      <img id="1" class="hangmanImages--img1 hangmanImages" src="img/hangmanImg/1.png" alt="" width=200 height=200>
+      <img id="2" class="hangmanImages--img2 hangmanImages" src="img/hangmanImg/2.png" alt="" width=200 height=200>
+      <img id="3" class="hangmanImages--img3 hangmanImages" src="img/hangmanImg/3.png" alt="" width=200 height=200>
+      <img id="4" class="hangmanImages--img4 hangmanImages" src="img/hangmanImg/4.png" alt="" width=200 height=200>
+      <img id="5" class="hangmanImages--img5 hangmanImages" src="img/hangmanImg/5.png" alt="" width=200 height=200>
+      <img id="6" class="hangmanImages--img6 hangmanImages" src="img/hangmanImg/6.png" alt="" width=200 height=200>
+      <img id="7" class="hangmanImages--img7 hangmanImages" src="img/hangmanImg/7.png" alt="" width=200 height=200>
+      <img id="8" class="hangmanImages--img8 hangmanImages" src="img/hangmanImg/8.png" alt="" width=200 height=200>
+      <img id="9" class="hangmanImages--img9 hangmanImages" src="img/hangmanImg/9.png" alt="" width=200 height=200>
+      <img id="10" class="hangmanImages--img10 hangmanImages" src="img/hangmanImg/10.png" alt="" width=200 height=200>
     </div>
 
     <div class="gridOutput">
